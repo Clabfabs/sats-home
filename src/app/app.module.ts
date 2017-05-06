fcb97461048d3c58ee0a2bf45ba20fc723e51f15
Fabio <islerfab@gmail.com>
2017-05-06 15:27:56 +0200

    Redesign of the homepage -> just single "Home" and "SATS Web Interface"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,10 @@ import {RouterModule, Routes} from "@angular/router";
 import {AppComponent} from "./app.component";
 import {NavbarComponent} from "./navbar/navbar.component";
 import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {GettingStartedComponent} from "./getting-started/getting-started.component";
-import {UsageComponent} from "./usage/usage.component";
-import {SatsCoreGettingStartedComponent} from "./getting-started/sats-core/sats-core.component";
-import {SatsCliGettingStartedComponent} from "./getting-started/sats-cli/sats-cli.component";
-import {SatsWebGettingStartedComponent} from "./getting-started/sats-web/sats-web.component";
-import {SatsCoreUsageComponent} from "./usage/sats-core/sats-core.component";
-import {SatsCliUsageComponent} from "./usage/sats-cli/sats-cli.component";
-import {SatsWebUsageComponent} from "./usage/sats-web/sats-web.component";
 import {SatsWebComponent} from './sats-web/sats-web.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'getting-started', component: GettingStartedComponent},
   {path: 'sats-web', component: SatsWebComponent}
 ];
 
@@ -30,15 +19,6 @@ const appRoutes: Routes = [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent,
-    GettingStartedComponent,
-    UsageComponent,
-    SatsCoreGettingStartedComponent,
-    SatsCliGettingStartedComponent,
-    SatsWebGettingStartedComponent,
-    SatsCoreUsageComponent,
-    SatsCliUsageComponent,
-    SatsWebUsageComponent,
     SatsWebComponent
   ],
   imports: [
